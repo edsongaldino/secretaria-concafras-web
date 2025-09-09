ff5984f4e1fcd1eb9f8b51b679f4b7ee39354309
Edson Galdino <edsongaldino@outlook.com>
2025-09-08 20:58:04 -0400

    remoção render mode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { UsuariosLista } from './features/usuarios/lista/lista';
 import { InscricaoForm } from './features/inscricao/inscricao-form/inscricao-form';
 import { EventoListComponent } from './features/eventos/evento-list/evento-list';
 import { EventoFormComponent } from './features/eventos/evento-form/evento-form';
-import { RenderMode } from '@angular/ssr';
 import { InscricaoList } from './features/inscricao/inscricao-list/inscricao-list';
 
 export const appRoutes: Routes = [
@@ -15,10 +14,10 @@ export const appRoutes: Routes = [
   { path: 'home', component: Home, canActivate: [AuthGuard] },
   { path: 'usuarios', component: UsuariosLista, canActivate: [AuthGuard] },
 
-  { path: 'inscricao/:eventoId', component: InscricaoForm, data: { renderMode: RenderMode.Client } },
+  { path: 'inscricao/:eventoId', component: InscricaoForm },
   { path: 'eventos', component: EventoListComponent, canActivate: [AuthGuard] },
-  { path: 'eventos/novo', component: EventoFormComponent, data: { renderMode: RenderMode.Client } },
-  { path: 'eventos/editar/:id', component: EventoFormComponent, data: { renderMode: RenderMode.Client } },
-  { path: 'eventos/:eventoId/inscricoes/:participanteId',component: InscricaoList},
-  { path: 'eventos/:eventoId/inscricao/nova/:responsavelId',component: InscricaoForm}
+  { path: 'eventos/novo', component: EventoFormComponent },
+  { path: 'eventos/editar/:id', component: EventoFormComponent },
+  { path: 'eventos/:eventoId/inscricoes/:participanteId', component: InscricaoList },
+  { path: 'eventos/:eventoId/inscricao/nova/:responsavelId', component: InscricaoForm }
 ];
